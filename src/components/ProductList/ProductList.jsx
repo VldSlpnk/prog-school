1215fa6565a11992c684ade41432c9c9eee887d9
sillyweeboo <defendervdele@gmail.com>
2024-08-07 13:09:59 +0300

    New update

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,6 +4,7 @@ import macbook from '../../img/macbook.svg'
 import './ProductList.css'
 import ReactPlayer from 'react-player'
 import PlayIcon from './PlayIcon'
+import Button from '../Button/Button'
 
 const ProductList = () => {
   return (
@@ -31,6 +32,44 @@ const ProductList = () => {
                 height="375px"
                 playing
               />
+            </div>
+          </div>
+        </div>
+        <div className="education-container">
+          <div className="education-text">
+            <div className="education-number">01</div>
+            <div className="education-title">
+              Весь материал разбит на небольшие уроки
+            </div>
+            <div className="education-subtitle">
+              Теория и практика подаются маленькими порциями. Так вам будет
+              легче усваивать новые знания.
+            </div>
+          </div>
+          <div className="education-text">
+            <div className="education-number">02</div>
+            <div className="education-title">Обучение через практику</div>
+            <div className="education-subtitle">
+              Всё, что вы узнали, вы тут же начинаете применять на практике. Вы
+              сразу видите результаты своего труда.
+            </div>
+          </div>
+          <div className="education-text">
+            <div className="education-number">03</div>
+            <div className="education-title">Нет ограничений по времени</div>
+            <div className="education-subtitle">
+              Можно совмещать учёбу с работой и другими делами. Не нужно
+              выпрашивать академический отпуск, если пришлось сделать перерыв.
+            </div>
+          </div>
+        </div>
+        <div className="start-education">
+          <div className="start-education-container">
+            <button className="start-education_button">
+              <span className="text-button">Начать обучение</span>
+            </button>
+            <div className="start-education_text">
+              Попробуйте, первые уроки бесплатны, но нужно пройти тестирование
             </div>
           </div>
         </div>
